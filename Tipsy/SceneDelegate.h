a238b3529837e6b8daaa40381b75354285a76886
Santy Mendoza <santymendoza@fb.com>
2021-06-22 12:05:57 -0700

    Initial Commit

---
+++ Tipsy/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Tipsy
+//
+//  Created by Santy Mendoza on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Tipsy/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Tipsy
+//
+//  Created by Santy Mendoza on 6/22/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
